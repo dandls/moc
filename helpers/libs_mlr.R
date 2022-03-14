a2c3754cdbe57fd7130736c786a0093150e1ef87
susanne-207 <dandl.susanne@gmail.com>
2022-03-14 11:59:12 +0100

    delete unnecessary folders

--- helpers/libs_mlr.R
+++ helpers/libs_mlr.R
@@ -15,9 +15,7 @@ if(length(new.packages)) install.packages(new.packages)
 # #--- Load respositories iml ----
 # load `iml` and `counterfactuals` like "normal" packages.
 # in the future this would just be library("counterfactuals").
-devtools::load_all("../iml", export_all = FALSE)
-devtools::install_github("dandls/counterfactuals")
-
+library("iml")
 library("counterfactuals")
 library("mlr")
 library("mlrCPO")
